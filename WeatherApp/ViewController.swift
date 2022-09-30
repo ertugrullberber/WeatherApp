4a7686cc9a378041f12e506e38cf779265e60963
ertugrullberber <ertugrulberber3461@gmail.com>
2022-09-30 19:46:32 +0300

    Added Screen

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn: UIButton!
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        btn.layer.cornerRadius = 6
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+        let city = textField.text ?? ""
+        
+        if city.isEmpty {
+            let alert = UIAlertController(title: "UYARI", message: "Lütfen şehir adı giriniz", preferredStyle: .alert)
+            
+            let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            alert.addAction(cancelButton)
+            
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            let vc = self.storyboard?.instantiateViewController(identifier: "") as! DetailViewController
+        }
+    }
+    
 }
 
